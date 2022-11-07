ded5af24aa3ff221de60ab6cc642ad5c6cbe1ee0
Vlad Shchelkunov <specter.ode@gmail.com>
2022-11-07 21:22:07 +0200

    text dotenv

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import instance from './helpers/api/auth';
 import { authStore } from 'mobxStores/stores';
 import { GoogleOAuthProvider } from '@moeindana/google-oauth';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Kapusta-Refactoring-MobX/">
-      <GoogleOAuthProvider clientId="360398004101-61pu4g5lqgm4i3oc66c0p0jssd3ofd2i.apps.googleusercontent.com">
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
     </BrowserRouter>
