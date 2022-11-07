e2e4d2562546a3b7e2372df7d9f1658c653364a6
Vlad Shchelkunov <specter.ode@gmail.com>
2022-11-07 21:26:34 +0200

    test

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import instance from './helpers/api/auth';
 import { authStore } from 'mobxStores/stores';
 import { GoogleOAuthProvider } from '@moeindana/google-oauth';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
