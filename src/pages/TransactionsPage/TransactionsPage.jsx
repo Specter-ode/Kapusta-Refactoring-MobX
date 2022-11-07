c9009e8d7606294d4761f61fe7c6a761f4523ff7
Vlad Shchelkunov <specter.ode@gmail.com>
2022-11-07 20:21:22 +0200

    remove unused-vars

--- src/pages/TransactionsPage/TransactionsPage.jsx
+++ src/pages/TransactionsPage/TransactionsPage.jsx
@@ -3,28 +3,16 @@ import { ReactComponent as BarChart } from 'assets/svg/bar_chart.svg';
 import Balance from 'components/Balance/Balance';
 import Dashboard from '../../components/Dashboard/Dashboard';
 import { Link, Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ExpensesAndIncome from 'components/Dashboard/ExpensesAndIncome/ExpensesAndIncome';
 import Calendar from 'components/Calendar/Calendar';
 import { observer } from 'mobx-react-lite';
-import { transactionStore, authStore } from 'mobxStores/stores';
 
 const TransactionsPage = () => {
   const { pathname } = useLocation();
   const vision = window.innerWidth > 767 || pathname === '/transactions';
   const calendarVision = window.innerWidth < 768 && pathname === '/transactions';
   const [date, setDate] = useState(() => new Date());
-  // const { getExpense, getIncome, getExpenseCategories, getIncomeCategories } = transactionStore;
-  // const { currentUser, accessToken } = authStore;
-  // useEffect(() => {
-  //   if (currentUser && accessToken) {
-  //     getExpense();
-  //     getIncome();
-  //     getExpenseCategories();
-  //     getIncomeCategories();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [accessToken, currentUser]);
 
   return (
     <main className={s.meta}>
